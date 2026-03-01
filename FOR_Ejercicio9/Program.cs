da651e4d9cd1f240fe6f26a9fecc85fcf5db0261
dgTr3 <Edugta31@gmail.com>
2026-03-01 14:29:02 -0600

    completo

--- FOR_Ejercicio9/Program.cs
+++ FOR_Ejercicio9/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Mostrar un patrón donde cada fila tenga el número de la fila repetido.");
+for (int i = 1; i <= 5; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+}
